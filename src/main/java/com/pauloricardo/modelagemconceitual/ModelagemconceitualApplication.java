696e0058bcf1bce0cee34c1f7ecb003394cf7963
Paulo Ricardo <prmsantosster@gmail.com>
2018-12-11 18:48:20 -0200

    Projeto Criado git status

---
+++ src/main/java/com/pauloricardo/modelagemconceitual/ModelagemconceitualApplication.java
@@ -0,0 +1,12 @@
+package com.pauloricardo.modelagemconceitual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModelagemconceitualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModelagemconceitualApplication.class, args);
+	}
+}
---
+++ src/test/java/com/pauloricardo/modelagemconceitual/ModelagemconceitualApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pauloricardo.modelagemconceitual;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ModelagemconceitualApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
