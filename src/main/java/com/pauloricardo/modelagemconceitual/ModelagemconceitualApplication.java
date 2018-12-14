ccce0dc8cdae0c07fc904a68e5bee418220f6a1b
Paulo Ricardo <prmsantosster@gmail.com>
2018-12-14 17:30:43 -0200

    Criado Operação de Instanciação.

--- src/main/java/com/pauloricardo/modelagemconceitual/ModelagemconceitualApplication.java
+++ src/main/java/com/pauloricardo/modelagemconceitual/ModelagemconceitualApplication.java
@@ -1,12 +1,32 @@
 package com.pauloricardo.modelagemconceitual;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ModelagemconceitualApplication {
+import com.pauloricardo.modelagemconceitual.domain.Categoria;
+import com.pauloricardo.modelagemconceitual.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class ModelagemconceitualApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ModelagemconceitualApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));		
+		
 	}
 }
