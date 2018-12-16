e34aaf58842d0118173990d40f92e51db698a27a
Paulo Ricardo <prmsantosster@gmail.com>
2018-12-16 15:24:01 -0200

    Ajustes no EndPoint /Categorias/{id}

--- src/main/java/com/pauloricardo/modelagemconceitual/services/CategoriaService.java
+++ src/main/java/com/pauloricardo/modelagemconceitual/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.pauloricardo.modelagemconceitual.domain.Categoria;
 import com.pauloricardo.modelagemconceitual.repositories.CategoriaRepository;
+import com.pauloricardo.modelagemconceitual.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -15,8 +16,9 @@ public class CategoriaService {
 	private CategoriaRepository repo;	
 
 	public Categoria find(Integer id) {
-		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		Optional<Categoria> obj = repo.findById(id);		
+			return obj.orElseThrow(() -> new ObjectNotFoundException(
+					"Objecto Não Encontrado !! Id: " + id + ", Tipo: " + Categoria.class.getName()));		
 	}
 
 }
