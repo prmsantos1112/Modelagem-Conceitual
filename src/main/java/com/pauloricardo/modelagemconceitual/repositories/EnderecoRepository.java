65a8a205127787c056192daf75958587a6864b3c
Paulo Ricardo <prmsantosster@gmail.com>
2018-12-16 19:17:53 -0200

    Cliente, TipoCliente, Telefones e Endereços

---
+++ src/main/java/com/pauloricardo/modelagemconceitual/repositories/EnderecoRepository.java
@@ -0,0 +1,11 @@
+package com.pauloricardo.modelagemconceitual.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.pauloricardo.modelagemconceitual.domain.Endereco;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Integer> {
+
+}
