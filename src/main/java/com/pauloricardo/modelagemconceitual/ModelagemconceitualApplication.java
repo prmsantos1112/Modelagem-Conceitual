edbe7741d71c2a88d908507f515a3dba2ee724e3
Paulo Ricardo <prmsantosster@gmail.com>
2018-12-17 10:56:17 -0200

    ItemPedido e ItemPedidoPK

--- src/main/java/com/pauloricardo/modelagemconceitual/ModelagemconceitualApplication.java
+++ src/main/java/com/pauloricardo/modelagemconceitual/ModelagemconceitualApplication.java
@@ -13,6 +13,7 @@ import com.pauloricardo.modelagemconceitual.domain.Cidade;
 import com.pauloricardo.modelagemconceitual.domain.Cliente;
 import com.pauloricardo.modelagemconceitual.domain.Endereco;
 import com.pauloricardo.modelagemconceitual.domain.Estado;
+import com.pauloricardo.modelagemconceitual.domain.ItemPedido;
 import com.pauloricardo.modelagemconceitual.domain.Pagamento;
 import com.pauloricardo.modelagemconceitual.domain.PagamentoComBoleto;
 import com.pauloricardo.modelagemconceitual.domain.PagamentoComCartao;
@@ -25,6 +26,7 @@ import com.pauloricardo.modelagemconceitual.repositories.CidadeRepository;
 import com.pauloricardo.modelagemconceitual.repositories.ClienteRepository;
 import com.pauloricardo.modelagemconceitual.repositories.EnderecoRepository;
 import com.pauloricardo.modelagemconceitual.repositories.EstadoRepository;
+import com.pauloricardo.modelagemconceitual.repositories.ItemPedidoRepository;
 import com.pauloricardo.modelagemconceitual.repositories.PagamentoRepository;
 import com.pauloricardo.modelagemconceitual.repositories.PedidoRepository;
 import com.pauloricardo.modelagemconceitual.repositories.ProdutoRepository;
@@ -55,6 +57,10 @@ public class ModelagemconceitualApplication implements CommandLineRunner{
 	
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
+	
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
+	
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ModelagemconceitualApplication.class, args);
@@ -119,7 +125,20 @@ public class ModelagemconceitualApplication implements CommandLineRunner{
 		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
 		
 		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
-		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));		
+		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));	
+		
+		ItemPedido ip1 = new ItemPedido(ped1, p1, 0.00, 1, 2000.00);
+		ItemPedido ip2 = new ItemPedido(ped1, p3, 0.00, 2, 80.00);
+		ItemPedido ip3 = new ItemPedido(ped2, p2, 100.00, 1, 800.00);
+		
+		ped1.getItens().addAll(Arrays.asList(ip1, ip2));
+		ped2.getItens().addAll(Arrays.asList(ip3));
+		
+		p1.getItens().addAll(Arrays.asList(ip1));
+		p2.getItens().addAll(Arrays.asList(ip3));
+		p3.getItens().addAll(Arrays.asList(ip2));
+		
+		itemPedidoRepository.saveAll(Arrays.asList(ip1, ip2, ip3));
 		
 	}
 }
